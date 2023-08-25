98502ab9c200601b8bfd64d2652c557f295df748
Artem Serbin <49901978+itsserbin@users.noreply.github.com>
2023-08-25 20:01:47 +0300

    Update seotools.php

--- config/seotools.php
+++ config/seotools.php
@@ -9,7 +9,7 @@ return [
          * The default configurations to be used by the meta generator.
          */
         'defaults'       => [
-            'title'       => "tehnobud.pl.ua", // set false to total remove
+            'title'       => "stop.pl.ua", // set false to total remove
             'titleBefore' => false, // Put defaults.title before page title, like 'It's Over 9000! - Dashboard'
             'description' => false, // set false to total remove
             'separator'   => ' - ',
